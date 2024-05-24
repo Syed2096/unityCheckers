14d69530de65885e066f98ec69ee27b129080ca9
syed2096 <amdsyd2000@gmail.com>
2024-05-23 23:17:29 -0400

    first commit

---
+++ Assets/GenerateBoard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBoard : MonoBehaviour
+{
+    public Board[,] pieces = new Board[8, 8];
+    public GameObject black;
+    public GameObject red;
+
+    void Start() {
+
+        for(int row = 0; row < 3; row++) {
+            for(int col = 0;)
+        }
+
+    }
+
+}
